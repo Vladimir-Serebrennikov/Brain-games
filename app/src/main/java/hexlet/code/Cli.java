8100e78e92db5106136fb6f398b8c76d690765ef
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-07-03 17:59:37 +0300

    touch Cli.java class

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,21 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void showName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        if (scanner.hasNextLine()) {
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name + "!");
+        }
+        else {
+            scanner.close();
+        }
+        scanner.close();
+
+
+
+    }
+}
