e51317934838b19bd96739fefa8a0020085b97c4
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-07-07 10:27:10 +0300

    select a game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Cli.showName();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: 1");
+
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 0:
+                System.out.println("Goodbye!");
+                break;
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                System.out.println("May I have your name?");
+                scanner.nextLine();
+                String name = scanner.nextLine();
+                System.out.println("Hello, " + name + "!");
+                break;
+        }
+        scanner.close();
+
     }
 }
