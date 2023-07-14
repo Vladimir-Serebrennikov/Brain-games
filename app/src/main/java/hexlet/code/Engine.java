1ee8ceb13635ac1f396bf2cc204281dd40173f91
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-07-14 13:10:25 +0300

    touch Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,91 @@
+package hexlet.code;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Engine {
+    public static void gameStructure(String gameName) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        if(gameName.equals("Even")) {
+            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        } else if (gameName.equals("Calc")) {
+            System.out.println("What is the result of the expression?");
+        }
+
+        int count = 0;
+        while (count < 3) {
+            if (gameName.equals("Even")) {
+                int number = random.nextInt(100);
+                System.out.println("Question: " + number);
+                String answer = scanner.nextLine();
+                System.out.println("Your answer: " + answer);
+                boolean isEven = (number % 2 == 0);
+                if (isEven && answer.equals("no") || (!isEven && answer.equals("yes"))) {
+                    System.out.println("'yes' is wrong answer ;). Correct answer was 'no'. Let's try again, " + name + "!");
+                    return;
+                } else if (isEven && answer.equals("yes") || (!isEven && answer.equals("no"))) {
+                    System.out.println("Correct!");
+                    count++;
+                } else if (!answer.equals("yes") || !answer.equals("no")) {
+                    System.out.println("'yes' is wrong answer ;). Correct answer was 'no'. Let's try again, " + name + "!");
+                    return;
+                }
+
+            }else if (gameName.equals("Calc")) {
+                int number1 = random.nextInt(100);
+                int number2 = random.nextInt(100);
+                int operation = random.nextInt(3);
+
+                char operator;
+                int result;
+                switch (operation) {
+                    case 0:
+                        operator = '+';
+                        result = number1 + number2;
+                        break;
+                    case 1:
+                        operator = '-';
+                        result = number1 - number2;
+                        break;
+                    case 2:
+                        operator = '*';
+                        result = number1 * number2;
+                        break;
+                    default:
+                        operator = '+';
+                        result = number1 + number2;
+                        break;
+                }
+                System.out.println("Question: " + number1 + " " + operator + " " + number2);
+                System.out.println("Your answer: ");
+                int userAnswer = scanner.nextInt();
+                String userInput = scanner.nextLine();
+                boolean isResult = result == userAnswer;
+                if (!isResult) {
+                    System.out.println("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was " + "'" + result +
+                            "'.");
+                    System.out.println("Let's try again, " + name + "!");
+                    return;
+                } else if (isResult) {
+                    System.out.println("Correct!");
+                    count++;
+                } else if (scanner.hasNextLine()){
+
+                    System.out.println("'" + userInput + "'" + " is wrong answer ;(. Correct answer was " + "'" + result +
+                            "'.");
+                    System.out.println("Let's try again, " + name + "!");
+                    return;
+                }
+            }
+
+        }
+        System.out.println("Congratulations, " + name + "!");
+
+    }
+
+
+}
