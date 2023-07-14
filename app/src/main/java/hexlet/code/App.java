63e03b3e9fa1b32c778eb392b0d8db31d75c922f
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-07-14 17:49:03 +0300

    added GSD in App menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 
@@ -12,6 +13,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         int choice = scanner.nextInt();
         System.out.println("Your choice: " + choice);
@@ -33,6 +35,9 @@ public class App {
                 break;
             case 3:
                 Calc.calculator();
+                break;
+            case 4:
+                GCD.findGSD();
                 break;
             default:
                 break;
