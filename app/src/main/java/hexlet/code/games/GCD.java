2ea3a5797d1003c0900976d891caf1cf80c778a9
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-07-14 17:47:48 +0300

    toucg GSD game

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,18 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class GCD {
+    public static int findGSD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return findGSD(b,a % b);
+        }
+    }
+
+    public static void findGSD() {
+        String question = "Find the greatest common divisor of given numbers.";
+        Engine.gameStructure("GSD", question);
+    }
+}
