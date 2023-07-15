b4950ab7643a68c65af925aa376b17ac8b2182eb
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-07-15 18:42:05 +0300

    added Progression game in Engine structure

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 import java.util.Random;
@@ -88,7 +89,30 @@ public class Engine {
                     System.out.println("Correct!");
                     count++;
                 }
+            } else if(gameName.equals("Progression")) {
+                int[] progression = Progression.generateProgression();
+                int hiddenIndex = Progression.hideNumber(progression);
+                System.out.print("Question: ");
+                for (int i = 0; i < progression.length; i++) {
+                    if (i == hiddenIndex) {
+                        System.out.print(".. ");
+                    } else {
+                        System.out.print(progression[i] + " ");
+                    }
+
+                }
+                System.out.print("\nYour answer: ");
+                int userAnswer = scanner.nextInt();
+                if (userAnswer == progression[hiddenIndex]) {
+                    System.out.println("Correct!");
+                    count++;
+                } else {
+                    System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + progression[hiddenIndex] + "'.");
+                    System.out.println("Let's try again, " + name + "!");
+                    return;
+                }
             }
+
 
         }
         System.out.println("Congratulations, " + name + "!");
